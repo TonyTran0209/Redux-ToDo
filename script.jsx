c0fd8e8868e093f71c995dfdf02f1b54969187b0
Tony Tran <phi.tran.0209@gmail.com>
2017-12-13 19:27:04 +0700

    14. Reducer Composition with Objects
    https://egghead.io/lessons/react-redux-reducer-composition-with-objects

--- script.jsx
+++ script.jsx
@@ -1,6 +1,4 @@
-//////////////////////////////////////////////////
-// CONTROLLER
-//////////////////////////////////////////////////
+/********** CONTROLLER **********/
 const todo = (state, action) => {
     switch (action.type) {
         case 'ADD_TODO':
@@ -39,104 +37,72 @@ const todos = (state = [], action) => {
     }
 };
 
-//////////////////////////////////////////////////
-// TEST FUNCTION: ADD
-//////////////////////////////////////////////////
-const testAddTodo = () => {
-    const stateBefore = [];
-    const action = {
-        type: 'ADD_TODO',
-        id: 0,
-        text: 'Learn Redux'
-    };
-    const stateAfter = [
-        {
-            id: 0,
-            text: 'Learn Redux',
-            completed: false
-        }
-    ];
-
-    // UnitTest: check reducer is pure function
-    deepFreeze(stateBefore);
-    deepFreeze(action);
-
-    // UnitTest: stateBefore + action = stateAfter
-    expect(
-        todos(stateBefore, action)
-    ).toEqual(stateAfter);
-};
-
-//////////////////////////////////////////////////
-// TEST FUNCTION: TOGGLE
-//////////////////////////////////////////////////
-const testToggleTodo = () => {
-    const stateBefore = [
-        {
-            id: 0,
-            text: 'Learn Redux',
-            completed: false
-        },
-        {
-            id: 1,
-            text: 'Go shopping',
-            completed: false
-        }
-    ];
-    const action = {
-        type: 'TOGGLE_TODO',
-        id: 1
-    };
-    const stateAfter = [
-        {
-            id: 0,
-            text: 'Learn Redux',
-            completed: false
-        },
-        {
-            id: 1,
-            text: 'Go shopping',
-            completed: true // change here
-        }
-    ];
-
-    // UnitTest: check reducer is pure function
-    deepFreeze(stateBefore);
-    deepFreeze(action);
-
-    // UnitTest: stateBefore + action = stateAfter
-    expect(
-        todos(stateBefore, action)
-    ).toEqual(stateAfter);
-};
-
-//////////////////////////////////////////////////
-// CALL FUNCTIONS
-//////////////////////////////////////////////////
-testAddTodo();
-testToggleTodo();
-console.log('All tests passed.') || displayInPreview('All tests passed.');
-
-
-//////////////////////////////////////////////////
-// ADDITIONAL FUNCTIONS
-//////////////////////////////////////////////////
-
-// Function exported from deep-freeze lib
-function deepFreeze(o) {
-    if (o === Object(o)) {
-        Object.isFrozen(o) || Object.freeze(o)
-        Object.getOwnPropertyNames(o).forEach(function (prop) {
-            prop === 'constructor' || deepFreeze(o[prop])
-        })
+/********** FILTER **********/
+const visibilityFilter = (state = 'SHOW_ALL',
+                          action) => {
+    switch (action.type) {
+        case 'SET_VISIBILITY_FILTER':
+            return action.filter;
+        default:
+            return state;
     }
-    return o
-}
+};
 
-// display in plunker preview
-function displayInPreview(string) {
-    var newDiv = document.createElement("div");
-    var newContent = document.createTextNode(string);
-    newDiv.appendChild(newContent);
-    document.body.appendChild(newDiv)
-}
+const todoApp = (state = {}, action) => {
+    return {
+        todos: todos(
+            state.todos,
+            action
+        ),
+        visibilityFilter: visibilityFilter(
+            state.visibilityFilter,
+            action
+        )
+    };
+};
+
+const {createStore} = Redux;
+const store = createStore(todoApp);
+
+/********** SHOW CONSOLE LOG **********/
+console.log('Initial state:');
+console.log(store.getState());
+console.log('--------------------------------------------------');
+
+console.log('Dispatching ADD_TODO.')
+store.dispatch({
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------------------------------------------');
+
+console.log('Dispatching ADD_TODO.');
+store.dispatch({
+    type: 'ADD_TODO',
+    id: 1,
+    text: 'Go shopping'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------------------------------------------');
+
+console.log('Dispatching TOGGLE_TODO.');
+store.dispatch({
+    type: 'TOGGLE_TODO',
+    id: 0
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------------------------------------------');
+
+console.log('Dispatching SET_VISIBILITY_FILTER');
+store.dispatch({
+    type: 'SET_VISIBILITY_FILTER',
+    filter: 'SHOW_COMPLETED'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------------------------------------------');
