afff4bdf50ab6037abc8aeee341183cf0e79fe5d
Tony Tran <phi.tran.0209@gmail.com>
2017-12-15 14:39:10 +0700

    20. Extracting Presentational Components (Todo, TodoList)
    https://egghead.io/lessons/react-redux-extracting-presentational-components-todo-todolist

--- src/App.js
+++ src/App.js
@@ -93,6 +93,40 @@ const FilterLink = ({
     );
 };
 
+
+const Todo = ({
+                  onClick,
+                  completed,
+                  text
+              }) => (
+    <li
+        onClick={onClick}
+        style={{
+            textDecoration:
+                completed ?
+                    'line-through' :
+                    'none'
+        }}
+    >
+        {text}
+    </li>
+);
+
+const TodoList = ({
+                      todos,
+                      onTodoClick
+                  }) => (
+    <ul>
+        {todos && todos.map(todo =>
+            <Todo
+                key={todo.id}
+                {...todo}
+                onClick={() => onTodoClick(todo.id)}
+            />
+        )}
+    </ul>
+);
+
 const getVisibleTodos = (todos,
                          filter) => {
     switch (filter) {
@@ -159,26 +193,15 @@ class TodoApp extends Component {
                 }}>
                     Add Todo
                 </button>
-                <ul>
-                    {/*componet receive ToDos as a prop*/}
-                    {visibleTodos && visibleTodos.map(todo =>
-                        <li key={todo.id}
-                            onClick={() => {
-                                store.dispatch({
-                                    type: 'TOGGLE_TODO',
-                                    id: todo.id
-                                });
-                            }}
-                            style={{
-                                textDecoration:
-                                    todo.completed ?
-                                        'line-through' :
-                                        'none'
-                            }}>
-                            {todo.text}
-                        </li>
-                    )}
-                </ul>
+                <TodoList
+                    todos={visibleTodos}
+                    onTodoClick={id =>
+                        store.dispatch({
+                            type: 'TOGGLE_TODO',
+                            id
+                        })
+                    }
+                />
             </div>
         );
     }
